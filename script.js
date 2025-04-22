a73634ecc7d9811ccdf8098b7be54fdd04297285
Katerina-99 <rakitinakaty555@gmail.com>
2025-04-22 15:05:02 +0300

    calculate positive and negative grades, convert to letters grades

--- script.js
+++ script.js
@@ -25,3 +25,37 @@ console.log(`Максимальный балл среди студентов: ${
 //минимальный балл среди студентов
 const minGrade = Math.min(...grades);
 console.log(`Минимальный балл среди студентов: ${minGrade}`);
+
+//вычисляем количество студентов получивших положительную оценку
+const positiveGrade = grades.filter((el) => {
+  return el >= 60;
+}).length;
+
+console.log(
+  `Количество студентов, получивших положительную оценку: ${positiveGrade}`
+);
+
+//вычисляем количество студентов получивших отрицательную оценку
+const negativeGrade = grades.filter((el) => el < 60).length;
+
+console.log(
+  `Количество студентов, получивших отрицательную оценку: ${negativeGrade}`
+);
+
+// преобразуем числовые оценки в буквенный формат оценок с помощью метода map
+const convertToLetters = (grade) => {
+  if (grade >= 80 && grade <= 100) {
+    return "A";
+  } else if (grade >= 60 && grade <= 79) {
+    return "B";
+  } else if (grade >= 40 && grade <= 59) {
+    return "C";
+  } else if (grade >= 20 && grade <= 39) {
+    return "D";
+  } else {
+    return "E";
+  }
+};
+
+const letterGrades = grades.map(convertToLetters);
+console.log(letterGrades);
